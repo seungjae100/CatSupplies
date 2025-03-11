b3bf6d91f13e41064cad1a519e6f70a432a9d347
seungjae100 <seungjaebaekmail@gmail.com>
2025-03-11 14:09:30 +0900

    First Commit

---
+++ src/test/java/com/web/catsupplies/CatSuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.catsupplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatSuppliesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
